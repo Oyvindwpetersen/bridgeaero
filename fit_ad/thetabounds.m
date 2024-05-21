7c6a34686a16367c2b95d780c7dd4dad4c557a1a
Øyvind <oyvind.w.petersen@ntnu.no>
2024-05-21 19:40:04 +0200

    bounds theta

---
+++ fit_ad/thetabounds.m
@@ -0,0 +1,25 @@
+function [theta0,theta_lb,theta_ub]=hypbounds(model)
+
+
+%%
+
+theta0(model.idx.d,1)=model.ini.d;
+theta0(model.idx.sigma_v,1)=model.ini.sigma_v;
+theta0(model.idx.sigma,1)=model.ini.sigma;
+theta0(model.idx.L,1)=model.ini.L;
+theta0(model.idx.alpha,1)=model.ini.alpha0;
+theta0(model.idx.abar,1)=model.ini.abar0;
+
+theta_lb(model.idx.d,1)=model.lb.d;
+theta_lb(model.idx.sigma_v,1)=model.lb.sigma_v;
+theta_lb(model.idx.sigma,1)=model.lb.sigma;
+theta_lb(model.idx.L,1)=model.lb.L;
+theta_lb(model.idx.alpha,1)=model.lb.alpha;
+theta_lb(model.idx.abar,1)=model.lb.abar;
+
+theta_ub(model.idx.d,1)=model.ub.d;
+theta_ub(model.idx.sigma_v,1)=model.ub.sigma_v;
+theta_ub(model.idx.sigma,1)=model.ub.sigma;
+theta_ub(model.idx.L,1)=model.ub.L;
+theta_ub(model.idx.alpha,1)=model.ub.alpha;
+theta_ub(model.idx.abar,1)=model.ub.abar;
