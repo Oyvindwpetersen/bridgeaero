d464353b150aa727d8795125a4afecee3a2bff7c
Øyvind <oyvind.w.petersen@ntnu.no>
2025-05-21 20:17:26 +0200

    bug fix K

--- fit_ad/noisecov.m
+++ fit_ad/noisecov.m
@@ -30,7 +30,7 @@ function [N,N_grad]=noisecov(data_matrix,model)
 %
 % -------------------------------------
 %
-% Model 3: separate noise, on aerodyn stiffness and damping
+% Model 3: separate noise, on stiffness and damping
 %
 % |y'| = |K^2*ADs|+|vs|
 %        |K*ADd|  +|vd|
@@ -38,7 +38,7 @@ function [N,N_grad]=noisecov(data_matrix,model)
 % |y|  = |K^2*ADs|+|vs|
 %        |K^2*ADd|+|K*vd|
 %
-% Cov(V)=diag(sigma1^2*I,K*sigma2^2*I)
+% Cov(V)=diag(sigma1^2*I,K^2*sigma2^2*I)
 %
 % -------------------------------------
 %
@@ -50,7 +50,7 @@ function [N,N_grad]=noisecov(data_matrix,model)
 % |y|  = |K^2*ADs|+|vs|
 %        |K^2*ADd|+|K*vd|+|wd|
 %
-% Cov(V)=diag(sigma1^2*I,K*sigma2^2*I)+diag(0,sigma3^2*I)
+% Cov(V)=diag(sigma1^2*I,K^2*sigma2^2*I)+diag(0,sigma3^2*I)
 %
 % -------------------------------------
 %
@@ -62,7 +62,7 @@ function [N,N_grad]=noisecov(data_matrix,model)
 % |y|  = |K^2*ADs|+|K^2*vs|
 %        |K^2*ADd|+|K^2*vd|
 %
-% Cov(V)=diag(K^2*sigma1^2*I,K^2*sigma2^2*I)
+% Cov(V)=diag(K^4*sigma1^2*I,K^4*sigma2^2*I)
 %
 % -------------------------------------
 %
@@ -71,7 +71,7 @@ function [N,N_grad]=noisecov(data_matrix,model)
 % |y|  = |K^2*ADs|+|K^2*vs|+|ws|
 %        |K^2*ADd|+|K^2*vd|+|wd|
 %
-% Cov(V)=diag(K^2*sigma1^2*I,K^2*sigma2^2*I)+diag(sigma3^2*I,sigma4^2*I)
+% Cov(V)=diag(K^4*sigma1^2*I,K^4*sigma2^2*I)+diag(sigma3^2*I,sigma4^2*I)
 %
 % -------------------------------------
 %
@@ -88,6 +88,8 @@ if strcmpi(model.noise,'model3') | strcmpi(model.noise,'model3w') | strcmpi(mode
     % K_diag=diag(K);
     K_diag=spdiags(K(:),0,numel(K),numel(K));
     K_squared_diag=K_diag.^2;
+    K_cubic_diag=K_diag.^3;
+    K_quad_diag=K_diag.^4;
 end
 
 %% Noise matrix
@@ -107,34 +109,34 @@ elseif strcmpi(model.noise,'model2')
 
 elseif strcmpi(model.noise,'model3')
 
-    N=blkdiag2(model.hyp.sigma_v(1)^2*In,model.hyp.sigma_v(2)^2*K_diag);
+    N=blkdiag2(model.hyp.sigma_v(1)^2*In,model.hyp.sigma_v(2)^2*K_squared_diag);
     
     N_grad{1}=blkdiag2(2*model.hyp.sigma_v(1)*In,Zn,'sparse');
-    N_grad{2}=blkdiag2(Zn,2*model.hyp.sigma_v(2)*K_diag,'sparse');
+    N_grad{2}=blkdiag2(Zn,2*model.hyp.sigma_v(2)*K_squared_diag,'sparse');
 
 elseif strcmpi(model.noise,'model3w')
 
-    N=blkdiag2(model.hyp.sigma_v(1)^2*In,model.hyp.sigma_v(2)^2*K_diag)...
+    N=blkdiag2(model.hyp.sigma_v(1)^2*In,model.hyp.sigma_v(2)^2*K_squared_diag)...
       +blkdiag2(Zn,model.hyp.sigma_v(3)^2*In);
     
     N_grad{1}=blkdiag2(2*model.hyp.sigma_v(1)*In,Zn,'sparse');
-    N_grad{2}=blkdiag2(Zn,2*model.hyp.sigma_v(2)*K_diag,'sparse');
+    N_grad{2}=blkdiag2(Zn,2*model.hyp.sigma_v(2)*K_squared_diag,'sparse');
     N_grad{3}=blkdiag2(Zn,2*model.hyp.sigma_v(3)*In,'sparse');
 
 elseif strcmpi(model.noise,'model4')
 
-    N=blkdiag2(model.hyp.sigma_v(1)^2*K_squared_diag,model.hyp.sigma_v(2)^2*K_squared_diag);
+    N=blkdiag2(model.hyp.sigma_v(1)^2*K_quad_diag,model.hyp.sigma_v(2)^2*K_quad_diag);
     
-    N_grad{1}=blkdiag2(2*model.hyp.sigma_v(1)*K_squared_diag,Zn,'sparse');
-    N_grad{2}=blkdiag2(Zn,2*model.hyp.sigma_v(2)*K_squared_diag,'sparse');
+    N_grad{1}=blkdiag2(2*model.hyp.sigma_v(1)*K_quad_diag,Zn,'sparse');
+    N_grad{2}=blkdiag2(Zn,2*model.hyp.sigma_v(2)*K_quad_diag,'sparse');
     
 elseif strcmpi(model.noise,'model4w')
 
-    N=blkdiag2(model.hyp.sigma_v(1)^2*K_squared_diag,model.hyp.sigma_v(2)^2*K_squared_diag)...
+    N=blkdiag2(model.hyp.sigma_v(1)^2*K_quad_diag,model.hyp.sigma_v(2)^2*K_quad_diag)...
      +blkdiag2(model.hyp.sigma_v(3)^2*In,model.hyp.sigma_v(4)^2*In);
     
-    N_grad{1}=blkdiag2(2*model.hyp.sigma_v(1)*K_squared_diag,Zn,'sparse');
-    N_grad{2}=blkdiag2(Zn,2*model.hyp.sigma_v(2)*K_squared_diag,'sparse');
+    N_grad{1}=blkdiag2(2*model.hyp.sigma_v(1)*K_quad_diag,Zn,'sparse');
+    N_grad{2}=blkdiag2(Zn,2*model.hyp.sigma_v(2)*K_quad_diag,'sparse');
     N_grad{3}=blkdiag2(model.hyp.sigma_v(3)^2*In,Zn,'sparse');
     N_grad{4}=blkdiag2(Zn,model.hyp.sigma_v(4)^2*In,'sparse');
     
