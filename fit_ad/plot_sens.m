07800c148b52dc926fbe3b59115c6ad6eb856974
Øyvind <oyvind.w.petersen@ntnu.no>
2025-05-21 20:17:40 +0200

    option normalize

--- fit_ad/plot_sens.m
+++ fit_ad/plot_sens.m
@@ -1,4 +1,5 @@
-function plot_sens(model,neg_logL_grad)
+function plot_sens(model,neg_logL_grad,varargin)
+
 %% Create options for model: kernel, noise, hyperparameters, bounds, ...
 %
 % Inputs:
@@ -10,44 +11,75 @@ function plot_sens(model,neg_logL_grad)
 
 %%
 
+p=inputParser;
+addParameter(p,'markersize',5,@isnumeric)
+addParameter(p,'normalize',true,@islogical)
+
+parse(p,varargin{:})
+
+markersize=p.Results.markersize;
+normalize=p.Results.normalize;
+
+%%
+
+plotopt=struct();
+plotopt.markersize=markersize;
+
 figure(); hold on; grid on;
+
+ylog;
+
+% i1=model{1,1}.idx.glob(model{1,1}.idx.d);
+% plot(i1,abs(neg_logL_grad(i1)),'ks',plotopt);
+% sens=abs(neg_logL_grad(i2));
+% if normalize==true; sens=sens./model{idx1,idx2}.hyp.sigma_v; end
+% plot(i2,sens,'ob',plotopt);
+
+i1=model{1,1}.idx.glob(model{1,1}.idx.d);
+sens=abs(neg_logL_grad(i1));
+if normalize==true; sens=sens./model{1,1}.hyp.d; end
+plot(i1,sens,'sk',plotopt);
 
 for idx1=1:size(model,1)
     for idx2=1:size(model,2)
 
-        i1=model{idx1,idx2}.idx.glob(model{idx1,idx2}.idx.d);
-        plot(i1,abs(neg_logL_grad(i1)),'ks');
-
         i2=model{idx1,idx2}.idx.glob(model{idx1,idx2}.idx.sigma_v);
-        plot(i2,abs(neg_logL_grad(i2)),'ob');
+        sens=abs(neg_logL_grad(i2));
+        if normalize==true; sens=sens./model{idx1,idx2}.hyp.sigma_v; end
+        plot(i2,sens,'ob',plotopt);
 
         i3=model{idx1,idx2}.idx.glob(model{idx1,idx2}.idx.sigma);
-        plot(i3,abs(neg_logL_grad(i3)),'xr');
+        sens=abs(neg_logL_grad(i3));
+        if normalize==true; sens=sens./model{idx1,idx2}.hyp.sigma; end
+        plot(i3,sens,'xr',plotopt);
 
         i4=model{idx1,idx2}.idx.glob(model{idx1,idx2}.idx.L);
-        plot(i4,abs(neg_logL_grad(i4)),'md');
+        sens=abs(neg_logL_grad(i4));
+        if normalize==true; sens=sens./model{idx1,idx2}.hyp.L; end
+        plot(i4,sens,'dm',plotopt);
 
         for k=1:length(i1)
-            yl{i1(k)}=['d' num2str(k)];
+            yl{i1(k)}=['d_' num2str(k)];
         end
         for k=1:length(i2)
-            yl{i2(k)}=['\sigma_v' num2str(k)];
+            yl{i2(k)}=['\sigma_{v,' num2str(k) '}'];
         end
         for k=1:length(i3)
-            yl{i3(k)}=['\sigma' num2str(k)];
+            yl{i3(k)}=['\sigma_' num2str(k)];
         end
 
         for k=1:length(i4)
-            yl{i4(k)}=['L' num2str(k)];
+            yl{i4(k)}=['L_' num2str(k)];
         end
         
     end
 end
 
-ylog;
-
 xlabel('Parameter');
-ylabel('Sensitivity');
+
+if normalize==true; ylabel({'Normalized' 'sensitivity'});
+else; ylabel({'Sensitivity'});
+end
 
 xticks([1:length(yl)]);
 xticklabels(yl);
